ed505643a5bfd47c75618243b85ed5a1b5523f53
Frank DeCaire <frank@decaire.net>
2017-02-11 14:39:30 -0500

    Initial Check-in

---
+++ ApiInterfaceTestConsole/CookieAwareWebClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ApiInterfaceTestConsole
+{
+	public class CookieAwareWebClient : WebClient
+	{
+		public CookieContainer CookieContainer { get; set; }
+
+		public CookieAwareWebClient()
+		{
+			CookieContainer = new CookieContainer();
+		}
+
+		public void SaveCookies(string filename)
+		{
+			var stream = File.Open(filename, FileMode.Create);
+			var formatter = new BinaryFormatter();
+
+			formatter.Serialize(stream, CookieContainer);
+			stream.Close();
+		}
+
+		public void LoadCookies(string filename)
+		{
+			var stream = File.Open(filename, FileMode.Open);
+			var formatter = new BinaryFormatter();
+
+			CookieContainer = (CookieContainer)formatter.Deserialize(stream);
+			stream.Close();
+		}
+
+		protected override WebRequest GetWebRequest(Uri address)
+		{
+			var request = base.GetWebRequest(address);
+
+			var webRequest = request as HttpWebRequest;
+			if (webRequest != null)
+			{
+				webRequest.CookieContainer = CookieContainer;
+			}
+
+			return request;
+		}
+	}
+}
---
+++ ApiInterfaceTestConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiInterfaceTestConsole
+{
+	class Program
+	{
+		private static string url = "http://localhost:5000";
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(GetValues());
+			Console.WriteLine(GetValueWithId());
+
+			Console.ReadKey();
+		}
+
+		private static object GetValues()
+		{
+			using (var webClient = new CookieAwareWebClient())
+			{
+				webClient.Headers["Accept-Encoding"] = "UTF-8";
+				webClient.Headers["Content-Type"] = "application/json";
+
+				var arr = webClient.DownloadData(url + "/api/values");
+				return Encoding.ASCII.GetString(arr);
+			}
+		}
+
+		private static object GetValueWithId()
+		{
+			using (var webClient = new CookieAwareWebClient())
+			{
+				webClient.Headers["Accept-Encoding"] = "UTF-8";
+				webClient.Headers["Content-Type"] = "application/json";
+
+				var arr = webClient.DownloadData(url + "/api/values/12");
+				return Encoding.ASCII.GetString(arr);
+			}
+		}
+	}
+}
